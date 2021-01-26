436aad071aa8e655f392d9876f5dfa7a1abe862c
Victor <55414928+victorleles12@users.noreply.github.com>
2021-01-26 17:41:54 -0300

    Victor

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import '../layout/Content.css'
+import Home from '../../views/examples/Home'
+import About from '../../views/examples/About'
+import { Switch, Route} from 'react-router-dom'
+import Param from '../../views/examples/Param'
 
 
 const Content = props => (
     <aside className="Content">
-        <h1>Componente Content</h1>
+        <Switch>
+            <Route path="/about">
+                <About />
+            </Route>
+            <Route path="/param/:id">
+                <Param />
+            </Route>
+            <Route path="/">
+                <Home />
+            </Route>
+        </Switch>
     </aside>
 )
 
